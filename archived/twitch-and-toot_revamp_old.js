717c3ad3480bd1939805b0b0ab3ebe968099d718
ChiefGyk3D <chiefgyk3d@pop-os.localdomain>
2023-02-04 19:49:07 -0500

    Managed to get Mastodon to post at most once every six hours, preventing multiple posts on stream. It will write a new txt file to do so. It also added an array to randomly select one of the five messages there.

--- archived/twitch-and-toot_revamp_old.js
+++ archived/twitch-and-toot_revamp_old.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 const { getKey } = require("./modules/auth.js");
 const { getData: getChannelData } = require("./modules/channelData.js");
 const { getData: getStreamData } = require("./modules/getStreams.js");
-const randomIndex = Math.floor(Math.random() * 5);
 
 // Define a list of messages to be posted randomly when the streamer is live
 const messages = [
@@ -15,28 +14,19 @@ const messages = [
   "Don't miss the live stream, it's happening now!"
 ];
 
-let lastPostTime = 0;
-
 async function postToMastodon(status) {
-  const currentTime = new Date().getTime();
+  const M = new mastodon({
+    access_token: config.mastodonAccessToken,
+    api_url: config.mastodonInstance + "/api/v1/"
+  });
 
-  if (currentTime - lastPostTime >= 6 * 60 * 60 * 1000) {
-    const M = new mastodon({
-      access_token: config.mastodonAccessToken,
-      api_url: config.mastodonInstance + "/api/v1/"
-    });
-
-    M.post("statuses", { status: status }, (error, data) => {
-      if (error) {
-        console.error(error);
-      } else {
-        console.log("Post to Mastodon successful!");
-        lastPostTime = currentTime;
-      }
-    });
-  } else {
-    console.log("Mastodon post skipped, last post was less than 6 hours ago.");
-  }
+  M.post("statuses", { status: status }, (error, data) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log("Post to Mastodon successful!");
+    }
+  });
 }
 
 async function checkStreamerStatus() {
@@ -70,21 +60,11 @@ async function checkStreamerStatus() {
     console.log(`${config.ChannelName} is live!`);
   }
 
-  // Check if it has been more than 6 hours since the last post
-  const lastPostTime = fs.existsSync("./lastPostTime.txt")
-    ? parseInt(fs.readFileSync("./lastPostTime.txt").toString(), 10)
-    : 0;
-  const now = new Date().getTime();
-  if (now - lastPostTime > 6 * 60 * 60 * 1000) {
     // Post to Mastodon
-    postToMastodon(messages[randomIndex]);
-
-    // Save the time of this post
-    fs.writeFileSync("./lastPostTime.txt", now.toString());
-  }
+    postToMastodon(message);
+  
 }
 
-
 // Check the streamer status every 10 minutes
-checkStreamerStatus();
+checkStreamerStatus()
 setInterval(checkStreamerStatus, 600000);
