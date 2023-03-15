a35883f56d6e7b534e7e49c7b5f4be9096b4a863
ChiefGyk3D <chiefgyk3d@pop-os.localdomain>
2023-03-15 14:51:35 -0400

    forgot a chunk of code

--- twitch-and-toot.js
+++ twitch-and-toot.js
@@ -31,12 +31,57 @@ try {
 
 let sendAnnouncement = false;
 
-// ... (rest of the code - postToMastodon function)
+async function postToMastodon(status) {
+  const currentTime = new Date().getTime();
+  const minMillisecondsBetweenPosts = config.minHoursBetweenPosts * 60 * 60 * 1000;
+
+  if (currentTime - lastPostTime >= minMillisecondsBetweenPosts) {
+    const M = new mastodon({
+      access_token: config.mastodonAccessToken,
+      api_url: config.mastodonInstance + "/api/v1/"
+    });
+
+    M.post("statuses", { status: status }, (error, data) => {
+      if (error) {
+        console.error(error);
+      } else if (!sendAnnouncement) {
+        console.log("Post to Mastodon successful!");
+        lastPostTime = currentTime;
+        fs.writeFile("lastPostTime.txt", lastPostTime, (err) => {
+          if (err) console.error("Error writing lastPostTime to file:", err);
+        });
+        sendAnnouncement = true;
+      }
+    });
+  } else {
+    console.log(`Mastodon post skipped, last post was less than ${config.minHoursBetweenPosts} hours ago.`);
+  }
+}
 
 async function checkStreamerStatus() {
-  // ... (rest of the code - checkStreamerStatus function before checking if streamer is live)
+  // Get Twitch API authentication token
+  const authToken = await getKey(config.twitch_clientID, config.twitch_secret);
 
-  // Check if the streamer is live
+  // Get channel data from Twitch API
+  const channelData = await getChannelData(
+    config.ChannelName,
+    config.twitch_clientID,
+    authToken
+  );
+
+  if (!channelData) {
+    console.error(`Channel "${config.ChannelName}" not found on Twitch.`);
+    return;
+  }
+
+  // Get stream data from Twitch API
+  const streamData = await getStreamData(
+    config.ChannelName,
+    config.twitch_clientID,
+    authToken
+  );
+
+// Check if the streamer is live
   if (streamData.data.length === 0) {
     console.log(`${config.ChannelName} is currently offline.`);
 
@@ -60,7 +105,19 @@ async function checkStreamerStatus() {
     }
   }
 
-  // ... (rest of the code - checkStreamerStatus function after checking if streamer is live)
+  // Extract the stream title and URL
+  const streamTitle = streamData.data[0].title;
+  const streamUrl = `https://www.twitch.tv/${config.ChannelName}`;
+
+  // Check if it has been more than the configured hours since the last post
+  if (!sendAnnouncement) {
+    // Post to Mastodon with the stream title and URL
+    const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+    const tootMessage = randomMessage
+      .replace("{streamTitle}", streamTitle)
+      .replace("{streamUrl}", streamUrl);
+    postToMastodon(tootMessage);
+  }
 }
 
 // Check the streamer status every 10 minutes
