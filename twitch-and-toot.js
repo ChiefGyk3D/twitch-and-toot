cc27967a47950f2bfe9babe9add642c9c8ec9d2a
ChiefGyk3D <chiefgyk3d@pop-os.localdomain>
2023-03-15 14:47:14 -0400

    Attempting to make a End of Stream notification for Mastodon

--- twitch-and-toot.js
+++ twitch-and-toot.js
@@ -1,4 +1,3 @@
-// Define a list of messages to be posted randomly when the streamer is live
 const mastodon = require("mastodon-api");
 const config = require("./config.json");
 const fs = require("fs");
@@ -7,78 +6,61 @@ const { getData: getChannelData } = require("./modules/channelData.js");
 const { getData: getStreamData } = require("./modules/getStreams.js");
 const messages = config.messages;
 
-let lastPostTime = 0;
-let sendAnnouncement = false;
-
-async function postToMastodon(status) {
-  const currentTime = new Date().getTime();
-  const minMillisecondsBetweenPosts = config.minHoursBetweenPosts * 60 * 60 * 1000;
-
-  if (currentTime - lastPostTime >= minMillisecondsBetweenPosts) {
-    const M = new mastodon({
-      access_token: config.mastodonAccessToken,
-      api_url: config.mastodonInstance + "/api/v1/"
-    });
-
-    M.post("statuses", { status: status }, (error, data) => {
-      if (error) {
-        console.error(error);
-      } else if (!sendAnnouncement) {
-        console.log("Post to Mastodon successful!");
-        lastPostTime = currentTime;
-        sendAnnouncement = true; // True, the post was successful.
-      }
-    });
-  } else {
-    console.log(`Mastodon post skipped, last post was less than ${config.minHoursBetweenPosts} hours ago.`);
-  }
+let lastPostTime;
+try {
+  lastPostTime = Number(fs.readFileSync("lastPostTime.txt", "utf-8"));
+  if (isNaN(lastPostTime)) lastPostTime = 0;
+} catch (error) {
+  lastPostTime = 0;
 }
 
+let prevStreamStatus;
+try {
+  prevStreamStatus = fs.readFileSync("streamStatus.txt", "utf-8");
+} catch (error) {
+  prevStreamStatus = "offline";
+}
+
+let lastOnlineTime;
+try {
+  lastOnlineTime = Number(fs.readFileSync("lastOnlineTime.txt", "utf-8"));
+  if (isNaN(lastOnlineTime)) lastOnlineTime = 0;
+} catch (error) {
+  lastOnlineTime = 0;
+}
+
+let sendAnnouncement = false;
+
+// ... (rest of the code - postToMastodon function)
+
 async function checkStreamerStatus() {
-  // Get Twitch API authentication token
-  const authToken = await getKey(config.twitch_clientID, config.twitch_secret);
-
-  // Get channel data from Twitch API
-  const channelData = await getChannelData(
-    config.ChannelName,
-    config.twitch_clientID,
-    authToken
-  );
-
-  if (!channelData) {
-    console.error(`Channel "${config.ChannelName}" not found on Twitch.`);
-    return;
-  }
-
-  // Get stream data from Twitch API
-  const streamData = await getStreamData(
-    config.ChannelName,
-    config.twitch_clientID,
-    authToken
-  );
+  // ... (rest of the code - checkStreamerStatus function before checking if streamer is live)
 
   // Check if the streamer is live
   if (streamData.data.length === 0) {
     console.log(`${config.ChannelName} is currently offline.`);
-    sendAnnouncement = false;
+
+    const currentTime = new Date().getTime();
+    const timeSinceLastOnline = currentTime - lastOnlineTime;
+    const thirtyMinutesInMilliseconds = 30 * 60 * 1000;
+
+    if (prevStreamStatus === "online" && timeSinceLastOnline <= thirtyMinutesInMilliseconds) {
+      const randomEndMessage = config.endOfStreamMessages[Math.floor(Math.random() * config.endOfStreamMessages.length)];
+      const endMessage = randomEndMessage.replace("{streamTitle}", streamTitle);
+      postToMastodon(endMessage);
+      fs.writeFileSync("streamStatus.txt", "offline");
+      sendAnnouncement = false;
+    }
     return;
   } else {
     console.log(`${config.ChannelName} is live!`);
+    if (prevStreamStatus === "offline") {
+      fs.writeFileSync("streamStatus.txt", "online");
+      fs.writeFileSync("lastOnlineTime.txt", new Date().getTime());
+    }
   }
 
-  // Extract the stream title and URL
-  const streamTitle = streamData.data[0].title;
-  const streamUrl = `https://www.twitch.tv/${config.ChannelName}`;
-
-  // Check if it has been more than the configured hours since the last post
-  if (!sendAnnouncement) {
-    // Post to Mastodon with the stream title and URL
-    const randomMessage = messages[Math.floor(Math.random() * messages.length)];
-    const tootMessage = randomMessage
-      .replace("{streamTitle}", streamTitle)
-      .replace("{streamUrl}", streamUrl);
-    postToMastodon(tootMessage);
-  }
+  // ... (rest of the code - checkStreamerStatus function after checking if streamer is live)
 }
 
 // Check the streamer status every 10 minutes
